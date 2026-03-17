070ab2c58bf7eb32c3f40afa172f9d6579e525af
Hamza Faheem <hamza.faheem@zekiexperts.com>
2026-03-17 23:54:57 +0500

    Dep Issue

--- src/server.js
+++ src/server.js
@@ -12,7 +12,9 @@ const adminRouter = require('./routes/admin');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+if (process.env.NODE_ENV === "production") {
+  app.set('trust proxy', 1);
+}
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
